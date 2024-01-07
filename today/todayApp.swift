1de71ac6c8c5012629a625a628557df066d79558
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-01-06 22:34:31 -0500

    Initial Commit

---
+++ today/todayApp.swift
@@ -0,0 +1,17 @@
+//
+//  todayApp.swift
+//  today
+//
+//  Created by Rafael on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct todayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
